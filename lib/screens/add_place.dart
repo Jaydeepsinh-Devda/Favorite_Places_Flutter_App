c413ae70cfa312be05385f99a9a85701820615bd
Jaydeepsinh Devda <djaybhai76@gmail.com>
2024-01-10 10:42:08 +0530

    Add riverpod pacakage

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -25,7 +25,7 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
         title: const Text('Add new Place'),
       ),
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(12),
+        padding: const EdgeInsets.all(12),
         child: Column(
           children: [
             TextField(
